2adaedeb93ac3178706644f629c7cb9260a0953d
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-10-16 18:57:50 -0300

    Bugs de duplicação e Update Enfermeiros resolvido

--- src/main/java/dao/EnfermeiroDao.java
+++ src/main/java/dao/EnfermeiroDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package dao;
 
 import factory.ConnectionFactory;
@@ -22,7 +21,7 @@ public class EnfermeiroDao {
             conn = ConnectionFactory.createConnectionToMySQL();
 
             //Criames uma PreparedStatement, para executar uma query
-            pstm =  conn.prepareStatement(sql);
+            pstm = conn.prepareStatement(sql);
             //Adicionar os valores que são esperados pela query
             pstm.setInt(1, enfermeiros.getCoren());
             pstm.setString(2, enfermeiros.getNome());
@@ -35,29 +34,29 @@ public class EnfermeiroDao {
             pstm.execute();
 
             System.out.println("Enfermeiros salvos com sucesso!");
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
 
             //Fechar as conexões
             try {
-                if(pstm != null) {
+                if (pstm != null) {
                     pstm.close();
                 }
 
-                if(conn != null) {
+                if (conn != null) {
                     conn.close();
                 }
-            }catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
 
 
+    public static void update(Enfermeiros enfermeiros) {
 
-    public void update(Enfermeiros enfermeiros) {
-
-        String sql = "UPDATE enfermeiros SET coren =?, nome = ?, sexo = ?, Especialidade = ?, telefone = ?, dataNasc = ?, uf = ? "
+        String sql = "UPDATE enfermeiros SET nome = ?, sexo = ?, Especialidade = ?, telefone = ?, dataNasc = ?, uf = ? "
                 + "WHERE coren = ?";
 
         Connection conn = null;
@@ -70,20 +69,20 @@ public class EnfermeiroDao {
             pstm = conn.prepareStatement(sql);
 
             //Adiciona valores para atualizar
-            pstm.setString(1, paciente.getNome());
-            pstm.setString(2, paciente.getCpf());
-            pstm.setString(3, paciente.getSexo());
-            pstm.setString(4, paciente.getTelefone());
-            pstm.setString(5, paciente.getDataNasc());
-            pstm.setString(6, paciente.getEndereco());
-            pstm.setString(7, paciente.getUf());
+            pstm.setString(1, enfermeiros.getNome());
+            pstm.setString(2, enfermeiros.getSexo());
+            pstm.setString(3, enfermeiros.getEspecialidade());
+            pstm.setString(4, enfermeiros.getTelefone());
+            pstm.setString(5, enfermeiros.getDataNasc());
+            pstm.setString(6, enfermeiros.getUf());
 
             //Qual o ID do registro que deseja atualizar
-            pstm.setInt(8, paciente.getProntuario());
+            pstm.setInt(7, enfermeiros.getCoren());
 
             //Executando a query
             pstm.execute();
-            System.out.println("Pacientes atualizado com sucesso!");
+            System.out.println("Enfermeiros atualizados com sucesso!");
+
         }catch(Exception e){
             e.printStackTrace();
         }finally {
@@ -101,108 +100,5 @@ public class EnfermeiroDao {
         }
     }
 
-=======
-package dao;
 
-import factory.ConnectionFactory;
-import model.Enfermeiros;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-public class EnfermeiroDao {
-    public static void save(Enfermeiros enfermeiros) {
-
-
-        String sql = "INSERT INTO enfermeiros(coren, nome, sexo, especialidade, telefone, dataNasc, uf) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-        Connection conn = null;
-        PreparedStatement pstm = null;
-
-        try {
-            //Criar uma conexão com o banco de dados
-            conn = ConnectionFactory.createConnectionToMySQL();
-
-            //Criames uma PreparedStatement, para executar uma query
-            pstm =  conn.prepareStatement(sql);
-            //Adicionar os valores que são esperados pela query
-            pstm.setInt(1, enfermeiros.getCoren());
-            pstm.setString(2, enfermeiros.getNome());
-            pstm.setString(3, enfermeiros.getSexo());
-            pstm.setString(4, enfermeiros.getEspecialidade());
-            pstm.setString(5, enfermeiros.getTelefone());
-            pstm.setString(6, enfermeiros.getDataNasc());
-            pstm.setString(7, enfermeiros.getUf());
-            //Executar a query
-            pstm.execute();
-
-            System.out.println("Enfermeiros salvos com sucesso!");
-        }catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-
-            //Fechar as conexões
-            try {
-                if(pstm != null) {
-                    pstm.close();
-                }
-
-                if(conn != null) {
-                    conn.close();
-                }
-            }catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-
-
-    public void update(Enfermeiros enfermeiros) {
-
-        String sql = "UPDATE enfermeiros SET coren =?, nome = ?, sexo = ?, Especialidade = ?, telefone = ?, dataNasc = ?, uf = ? "
-                + "WHERE coren = ?";
-
-        Connection conn = null;
-        PreparedStatement pstm = null;
-        try {
-            //Criar conexão com o banco
-            conn = ConnectionFactory.createConnectionToMySQL();
-
-            //Criar a classe para executar a query
-            pstm = conn.prepareStatement(sql);
-
-            //Adiciona valores para atualizar
-            pstm.setString(1, paciente.getNome());
-            pstm.setString(2, paciente.getCpf());
-            pstm.setString(3, paciente.getSexo());
-            pstm.setString(4, paciente.getTelefone());
-            pstm.setString(5, paciente.getDataNasc());
-            pstm.setString(6, paciente.getEndereco());
-            pstm.setString(7, paciente.getUf());
-
-            //Qual o ID do registro que deseja atualizar
-            pstm.setInt(8, paciente.getProntuario());
-
-            //Executando a query
-            pstm.execute();
-            System.out.println("Pacientes atualizado com sucesso!");
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally {
-            try {
-                if(pstm != null) {
-                    pstm.close();
-                }
-
-                if(conn != null) {
-                    conn.close();
-                }
-            }catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
->>>>>>> 347d89c1083d6b802abb287ddf0db7bb037bc5ff
-}        
+}
--- src/main/java/model/Enfermeiros.java
+++ src/main/java/model/Enfermeiros.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public class Enfermeiros {
@@ -66,72 +65,3 @@ public class Enfermeiros {
         this.uf = uf;
     }
 }
-=======
-package model;
-
-public class Enfermeiros {
-    private int coren;
-    private String nome;
-    private String sexo;
-    private String especialidade;
-    private String telefone;
-    private String dataNasc;
-    private String uf;
-
-    public int getCoren() {
-        return coren;
-    }
-
-    public void setCoren(int coren) {
-        this.coren = coren;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade(String especialidade) {
-        this.especialidade = especialidade;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getDataNasc() {
-        return dataNasc;
-    }
-
-    public void setDataNasc(String dataNasc) {
-        this.dataNasc = dataNasc;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-}
->>>>>>> 347d89c1083d6b802abb287ddf0db7bb037bc5ff
