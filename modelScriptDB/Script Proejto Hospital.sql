37e45ae80fd22feb01311f8633b1eca6c41ab232
Kauan Carolino <98748289+KauanCarolino@users.noreply.github.com>
2023-10-30 21:20:49 -0300

    Update Script Proejto Hospital.sql
    Erros de identação

--- modelScriptDB/Script Proejto Hospital.sql
+++ modelScriptDB/Script Proejto Hospital.sql
@@ -6,42 +6,42 @@ USE dbHospital;
 
 #Criação da tabela pacientes
 CREATE TABLE pacientes(
-                          Prontuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                          Nome VARCHAR(200),
-                          CPF VARCHAR(11) NOT NULL,
-                          Sexo ENUM("M", "F"),
-                          Telefone VARCHAR(10),
-                          dataNasc DATE,
-                          Endereco VARCHAR(200),
-                          UF CHAR(2)
+	Prontuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Nome VARCHAR(200),
+	CPF VARCHAR(11) NOT NULL,
+	Sexo ENUM("M", "F"),
+	Telefone VARCHAR(10),
+	dataNasc DATE,
+	Endereco VARCHAR(200),
+	UF CHAR(2)
 );
 
 #Criação da tabela medicos
 CREATE TABLE medicos(
-                        CRM INT(6) PRIMARY KEY,
-                        Nome VARCHAR(200),
-                        Sexo ENUM("M","F"),
-                        Senha VARCHAR(255),
-                        Especialidade VARCHAR(100),
-                        Telefone VARCHAR(10),
-                        dataNasc DATE,
-                        UF CHAR(2),
-                        idPasc INT,
-                        FOREIGN KEY (idPasc) REFERENCES pacientes(Prontuario)
+	CRM INT(6) PRIMARY KEY,
+	Nome VARCHAR(200),
+	Sexo ENUM("M","F"),
+	Senha VARCHAR(255),
+	Especialidade VARCHAR(100),
+	Telefone VARCHAR(10),
+	dataNasc DATE,
+	UF CHAR(2),
+	idPasc INT,
+	FOREIGN KEY (idPasc) REFERENCES pacientes(Prontuario)
 );
 
 #Criação da tabela enfermeiros
-CREATE TABLE enfermeiros(
-                            COREN INT(6) PRIMARY KEY,
-                            Nome VARCHAR(200),
-                            Sexo ENUM("M","F"),
-                            Senha VARCHAR(255),
-                            Especialidade VARCHAR(100),
-                            Telefone VARCHAR(10),
-                            dataNasc DATE,
-                            UF CHAR(2),
-                            idPasc INT,
-                            FOREIGN KEY (idPasc) REFERENCES pacientes(Prontuario)
+	CREATE TABLE enfermeiros(
+	COREN INT(6) PRIMARY KEY,
+	Nome VARCHAR(200),
+	Sexo ENUM("M","F"),
+	Senha VARCHAR(255),
+	Especialidade VARCHAR(100),
+	Telefone VARCHAR(10),
+	dataNasc DATE,
+	UF CHAR(2),
+	idPasc INT,
+        FOREIGN KEY (idPasc) REFERENCES pacientes(Prontuario)
 );
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123321';
