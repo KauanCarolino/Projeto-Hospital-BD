8b3d6194b45bfdea75f574b4182706ea33135a49
Santiago Nogueira <118860480+santiagoNogueira66@users.noreply.github.com>
2023-10-31 17:34:56 -0300

    upload da tela cadastro paciente

---
+++ src/main/java/interfaceFX/CadastroPaciente.java
@@ -0,0 +1,67 @@
+package com.example.trampodebd;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CadastroPaciente extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage paciente) throws Exception {
+        paciente.setTitle("cadastro paciente");
+        VBox root= new VBox(10);
+
+        /*cria uma cena com todos os elementos, onde a largura e 300px e
+        altura 200px respctivamente*/
+        Scene scene = new Scene(root, 300,350);
+
+        //caixa nome prontuario
+        TextField prontuario = new TextField();
+        prontuario.setPromptText("Prontuario");
+
+        //caixa nome
+        TextField nome = new TextField();
+        nome.setPromptText("Nome");
+
+
+        //caixa SEXO
+        TextField sexo = new TextField();
+        sexo.setPromptText("Sexo");
+
+       
+        //caixa telefone
+        TextField telefone = new TextField();
+        telefone.setPromptText("Telefone");
+
+        //caixa data de nascimento
+        DatePicker dataNascimento = new DatePicker();
+        dataNascimento.setPromptText("Data de Nascimento");
+
+        //caixa UF
+        TextField UF = new TextField();
+        UF.setPromptText("UF");
+
+        Button botaoCadastrar = new Button("Cadastrar");
+
+
+        /*mostra as caixas com usuario, senha e o
+        demais paramentros*/
+        root.getChildren().addAll(prontuario,nome,sexo,especialidade,
+                telefone,dataNascimento,UF,botaoCadastrar);
+
+
+
+        //faz com que as caixas sejam exibidas
+        paciente.setScene(scene);
+        paciente.show();
+    }
+}
+
