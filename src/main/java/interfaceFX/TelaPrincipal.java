a08d07cc70a59aa56c48b466da03bf0d114f079a
Santiago Nogueira <118860480+santiagoNogueira66@users.noreply.github.com>
2023-11-04 14:06:39 -0300

    erro na linha 47, não conseguir arrumar

---
+++ src/main/java/interfaceFX/TelaPrincipal.java
@@ -0,0 +1,51 @@
+package com.example.trampodebd;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TelaPrincipal extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start (Stage stage) throws Exception{
+    stage.setTitle("Tela principal");
+
+        VBox root = new VBox();
+        Scene scene = new Scene(root,300,200);
+        Label pesquisar  =  new Label("pesquisar: ");
+        TextField textField = new TextField();
+        root.setSpacing(10);
+
+        Button botaoPesquisar = new Button("pesquisar");
+
+        Button botaoNovoPct = new Button("novo paciente");
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+
+        root.getChildren().addAll(pesquisar, textField,botaoPesquisar,botaoNovoPct);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void acaoDoBotao(){
+        HBox root = new HBox();
+        Scene scene = new Scene(root,300,200);
+        Stage paciente = new Stage();
+
+        TelaCadastroPaciente instanciaTelaCadastroPaciente =
+         new TelaCadastroPaciente();
+
+        Button botaoPesquisar = new Button();
+
+        Button botaoNovoPaciente = new Button();
+       /*botaoNovoPaciente.setOnAction(e -> instanciaTelaCadastroPaciente.start
+       (paciente)); 👈 com erro*/
+        root.getChildren().addAll(botaoPesquisar,botaoNovoPaciente);
+    }
+}
