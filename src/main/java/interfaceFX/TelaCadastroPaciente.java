1e75ac2b77cdcd402307612fc651f608678d9b6f
Kauan Carolino <kauancarolino248@gmail.com>
2023-11-06 23:36:17 -0300

    Listagem, cadastro, remoção e edição de pacientes pronto

--- src/main/java/interfaceFX/TelaCadastroPaciente.java
+++ src/main/java/interfaceFX/TelaCadastroPaciente.java
@@ -3,12 +3,11 @@ package interfaceFX;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import dao.PacientesDao;
+import model.Pacientes;
 
 public class TelaCadastroPaciente extends Application {
     public static void main(String[] args) {
@@ -18,58 +17,80 @@ public class TelaCadastroPaciente extends Application {
     @Override
     public void start(Stage novoPCT) throws Exception {
         novoPCT.setTitle("Cadastro paciente");
-        VBox root= new VBox(10);
+        VBox root = new VBox(10);
 
-        /*cria uma cena com todos os elementos, onde a largura e 300px e
-        altura 200px respctivamente*/
-        Scene scene = new Scene(root, 300,470);
+        // Crie uma cena com todos os elementos, onde a largura é de 300px e a altura de 470px.
+        Scene scene = new Scene(root, 300, 470);
 
-        //caixa nome prontuario
-        Label PRONTUARIO = new Label("Prontuario");
-        TextField prontuario = new TextField();
+        // Crie os campos de entrada para os dados do paciente.
 
-
-        //caixa nome
         Label name = new Label("Nome");
         TextField nome = new TextField();
 
+        Label CPF = new Label("Cpf");
+        TextField cpf = new TextField();
 
-
-        //caixa SEXO
         Label SEXO = new Label("Sexo");
         TextField sexo = new TextField();
 
-
-
-        //caixa telefone
         Label TELEFONE = new Label("Telefone");
         TextField telefone = new TextField();
 
+        Label DATANASCIMENTO = new Label("Data Nascimento");
+        TextField dataNascimento = new TextField();
 
-        //caixa data de nascimento
+        Label ENDERECO = new Label("Endereço");
+        TextField endereco = new TextField();
 
-
-        //caixa UF
-        Label UF  = new Label("UF");
+        Label UF = new Label("UF");
         TextField uf = new TextField();
 
-        Label DATANASCIMENTO = new Label("Data de nascimento");
-        DatePicker dataNascimento = new DatePicker();
 
+        // Crie um botão para cadastrar o paciente.
         Button botaoCadastrar = new Button("Cadastrar");
 
+        botaoCadastrar.setOnAction(e -> {
+            // Crie um novo objeto Pacientes com os dados do formulário.
+            Pacientes novoPaciente = new Pacientes();
+            novoPaciente.setNome(nome.getText());
+            novoPaciente.setCpf(cpf.getText());
+            novoPaciente.setSexo(sexo.getText());
+            novoPaciente.setTelefone(telefone.getText());
+            novoPaciente.setDataNasc(dataNascimento.getText());
+            novoPaciente.setEndereco(endereco.getText());
+            novoPaciente.setUf(uf.getText());
 
-        /*mostra as caixas com usuario, senha e o
-        demais paramentros*/
-        root.getChildren().addAll(PRONTUARIO,prontuario,name,nome,SEXO,sexo,
-        UF,uf,TELEFONE,telefone,DATANASCIMENTO,dataNascimento,botaoCadastrar);
 
-        root.setAlignment(javafx.geometry.Pos.CENTER);
+            // Crie uma instância do PacientesDao para salvar o novo paciente.
+            PacientesDao pacientesDao = new PacientesDao();
+            pacientesDao.save(novoPaciente);
 
+            // Limpe os campos do formulário após o cadastro.
+            nome.clear();
+            cpf.clear();
+            sexo.clear();
+            telefone.clear();
+            dataNascimento.clear();
+            endereco.clear();
+            uf.clear();
+        });
 
-        //faz com que as caixas sejam exibidas
+        // Adicione os elementos à cena.
+        root.getChildren().addAll(
+                name, nome,
+                CPF, cpf,
+                SEXO, sexo,
+                UF, uf,
+                TELEFONE, telefone,
+                DATANASCIMENTO, dataNascimento,
+                ENDERECO, endereco,
+                botaoCadastrar
+        );
+
+        root.setAlignment(Pos.CENTER);
+
+        // Exiba a cena.
         novoPCT.setScene(scene);
         novoPCT.show();
     }
 }
-
